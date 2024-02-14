77fa634e54e0e37eb127355d9afc867e2be097de
Sayemum Hassan <sayemumhassan@gmail.com>
2024-02-14 14:29:23 -0500

    CS347 Prep 3 Done

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
         route='',
         view=views.CheeseListView.as_view(),
         name='list'
-    )
+    ),
+    # URL Pattern for the CheeseDetailView
+    path(
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
+    ),
 ]
