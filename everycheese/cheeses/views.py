b57ec8a2f1a7dd86ac67aae0ecbf50feab989209
Sayemum Hassan <sayemumhassan@gmail.com>
2024-02-23 15:52:02 -0500

    Logged in users only can access

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -2,12 +2,14 @@ from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Cheese
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class CheeseListView(ListView):
     model = Cheese
 
 class CheeseDetailView(DetailView):
     model = Cheese
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
     fields = ['name', 'description', 'firmness', 'country_of_origin']
